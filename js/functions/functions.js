ecc837ad2f17d286320673d4aa710130b79d0fbd
gabit <gabit690@gmail.com>
2021-11-20 23:54:18 -0300

    Axes functionality refactoring

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -17,5 +17,65 @@ export default {
             "december"
         ];
         return months[number - 1];
+    },
+    createSvgContainer: (parent = "body") => {
+        return d3.select(parent).append("svg");
+    },
+    getSvgWidth: (svgElement = {}) => {
+        return Number(svgElement.style("width").slice(0,-2));
+    },
+    getSvgHeight: (svgElement = {}) => {
+        return Number(svgElement.style("height").slice(0,-2));
+    },
+    createYearsScale: (dataset, width, padding) => {
+        return d3.scaleLinear()
+                    .domain([d3.min(dataset, (d) => d.year), d3.max(dataset, (d) => d.year)])
+                    .range([padding, width - padding]);
+    },
+    createMonthsScale: (height, padding) => {
+        return d3.scaleBand()
+                    .domain([
+                        "january", 
+                        "february", 
+                        "march",
+                        "april", 
+                        "may", 
+                        "june",
+                        "july",
+                        "august",
+                        "september",
+                        "october",
+                        "november",
+                        "december"
+                    ])
+                    .range([height - padding, padding / 2]);
+    },
+    addYearsAxis: (svgElement = {}, axisTitle, yearsScale, horizontalDisplacement) => {
+        let xAxis = d3.axisBottom(yearsScale).ticks(26);
+        
+        svgElement.append("g")
+            .attr("transform", `translate(0, ${horizontalDisplacement})`)
+            .call(xAxis);
+    
+        svgElement.append("text")
+            .attr("x", "50%")
+            .attr("y", horizontalDisplacement + 40)
+            .attr("dominant-baseline", "middle")
+            .attr("text-anchor", "middle")
+            .text(axisTitle);
+    },
+    addMonthsAxis: (svgElement = {}, axisTitle, monthsScale, verticalDisplacement) => {
+        let yAxis = d3.axisLeft(monthsScale);
+
+        svgElement.append("g")
+            .attr("transform", `translate(${verticalDisplacement}, 0)`)
+            .call(yAxis);
+    
+        svgElement.append("text")
+            .attr("x", "-20%")
+            .attr("y", verticalDisplacement - 70)
+            .attr("transform", "rotate(270)")
+            .text(axisTitle);
     }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,55 +1,15 @@
 import functions from './functions/functions.js';
 
 const showGraph = (dataset, baseTemperature) => {
-    let svg = d3.select("main").append("svg");
-    const WIDTH = Number(svg.style("width").slice(0,-2));
-    const HEIGHT = Number(svg.style("height").slice(0,-2));
+    let svg = functions.createSvgContainer("main");
+    const WIDTH = functions.getSvgWidth(svg);
+    const HEIGHT = functions.getSvgHeight(svg);
     const PADDING = 140;
-
-    let xScale = d3.scaleLinear()
-                    .domain([d3.min(dataset, (d) => d.year), d3.max(dataset, (d) => d.year)])
-                    .range([PADDING, WIDTH - PADDING]);
-
-    let yScale = d3.scaleBand()
-                    .domain([
-                        "january", 
-                        "february", 
-                        "march",
-                        "april", 
-                        "may", 
-                        "june",
-                        "july",
-                        "august",
-                        "september",
-                        "october",
-                        "november",
-                        "december"
-                    ])
-                    .range([HEIGHT - PADDING, PADDING / 2]);
-
-    let xAxis = d3.axisBottom(xScale).ticks(26);
-    let yAxis = d3.axisLeft(yScale);
-
-    svg.append("g")
-        .attr("transform", `translate(0, ${HEIGHT - PADDING})`)
-        .call(xAxis);
-
-    svg.append("text")
-        .attr("x", "50%")
-        .attr("y", HEIGHT - PADDING + 40)
-        .attr("dominant-baseline", "middle")
-        .attr("text-anchor", "middle")
-        .text("years");
-
-    svg.append("g")
-        .attr("transform", `translate(${PADDING}, 0)`)
-        .call(yAxis);
-
-    svg.append("text")
-        .attr("x", "-20%")
-        .attr("y", PADDING - 70)
-        .attr("transform", "rotate(270)")
-        .text("months");
+    let xScale = functions.createYearsScale(dataset, WIDTH, PADDING);
+    let yScale = functions.createMonthsScale(HEIGHT, PADDING);
+    // Use dataset to build heatMap
+    functions.addYearsAxis(svg, "years", xScale, (HEIGHT - PADDING));
+    functions.addMonthsAxis(svg, "months", yScale, PADDING);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
