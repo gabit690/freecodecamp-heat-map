73c0b0d43665279527660f3bed865f1ffb2ec462
gabit <gabit690@gmail.com>
2021-11-23 17:20:45 -0300

    colored graph cells

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -14,22 +14,79 @@ const MONTHS = [
 ];
 
 const COLORS = [
-    "#000080",
-    "#4D55AA",
-    "#9AA9D5",
-    "#E7FEFF",
-    "#FFFFE0",
-    "#E5B5A1",
-    "#CC6C61",
-    "#B22222"
-]
+    {
+        color: "#4575B4",
+        range: [1, 2.5]
+    },
+    {
+        color: "#74ADD1",
+        range: [2.5, 4]
+    },
+    {
+        color: "#ABD9E9",
+        range: [4, 5.5]
+    },
+    {
+        color: "#E0F3F8",
+        range: [5.5, 7]
+    },
+    {
+        color: "#FFFFBF",
+        range: [7, 8.5]
+    },
+    {
+        color: "#FEE090",
+        range: [8.5, 10]
+    },
+    {
+        color: "#FDAE61",
+        range: [10, 11.5]
+    },
+    {
+        color: "#F46D43",
+        range: [11.5, 13]
+    },
+    {
+        color: "#D73027",
+        range: [13, 14.5]
+    }
+];
+
+const monthToNumber = (month = "january") => {
+        return (MONTHS.indexOf(month));
+};
+    
+const roundedDecimal = (number = 0) => {
+    return (Math.round(number * 10) / 10);
+};
+
+const numberInRange = (number = 0, range = [0, 1]) => {
+    return number >= range[0] && number < range[1];
+};
+
+const temperatureToColor = (temperature = 10) => {
+    let color = "#aaa";
+    let colorWasFound = false;
+    let indexOfColors = 0;
+    let numberOfColor = COLORS.length;
+    do {
+        colorWasFound = numberInRange(temperature, COLORS[indexOfColors].range);
+        if(colorWasFound)
+            color = COLORS[indexOfColors].color;
+        indexOfColors++;
+    } while(!colorWasFound && indexOfColors < numberOfColor);
+    return color;
+};
+
+const temperatureFormatted = (temperature = 0) => {
+    return `${temperature.toFixed(1)}°C`;
+};
+
+const temperatureVarianceFormatted = (variance = 0) => {
+    return `${variance >= 0 ? "+":""}${variance.toFixed(1)}°C`;
+};
 
 export default {
-    numberToMonth: (number) => {
-        if(isNaN(number) || number < 1 || number > 12)
-            return "";
-        return MONTHS[number - 1];
-    },
     createSvgContainer: (parent = "body") => {
         return d3.select(parent).append("svg");
     },
@@ -41,7 +98,7 @@ export default {
     },
     createYearsScale: (dataset, width, padding) => {
         return d3.scaleLinear()
-                    .domain([d3.min(dataset, (d) => d.year), d3.max(dataset, (d) => d.year)])
+                    .domain([d3.min(dataset, (d) => d.year) - 1, d3.max(dataset, (d) => d.year) + 1])
                     .range([padding, width - padding]);
     },
     createMonthsScale: (height, padding) => {
@@ -80,27 +137,54 @@ export default {
                     .attr("fill", "black")
                     .text(axisTitle);
     },
+    getGraphDataset: (dataset, baseTemperature) => {
+        let graphDataset = dataset.map(element => {
+            return {
+                year: element.year,
+                month: MONTHS[element.month - 1],
+                temperature: roundedDecimal(baseTemperature + element.variance),
+                variance: roundedDecimal(element.variance)
+            };
+        });
+        return graphDataset;
+    },
+    buildHeatMap: (svgElement = {}, xScale, yScale, graphData) => {
+        svgElement.selectAll("rect")
+                    .data(graphData)
+                    .enter()
+                    .append("rect")
+                    .attr("class", "cell")
+                    .attr("data-month", (d, i) => monthToNumber(d.month))
+                    .attr("data-year", (d, i) => d.year)
+                    .attr("data-temp", (d, i) => d.temperature)
+                    .attr("color-code", (d, i) => temperatureToColor(d.temperature))
+                    .attr("x", (d, i) => xScale(d.year) - 2.5)
+                    .attr("y", (d, i) => yScale(d.month))
+                    .attr("width", 5)
+                    .attr("height", 31)
+                    .attr("fill", (d, i) => temperatureToColor(d.temperature));
+    },
     addTemperatureReference: (svgElement = {}, height, padding) => {
         
-        let xScale = d3.scaleLinear([0.0, 17.0], [padding, padding + (COLORS.length * 50)]);
+        let xScale = d3.scaleLinear([0.0, 15.5], [padding, padding + (COLORS.length * 50)]);
         let xAxis = d3.axisBottom(xScale)
-                        .tickValues([0.0, 2.5, 4, 5.5, 7, 8.5, 10, 11.5, 13, 14.5, 17.0])
-                        .tickFormat(t => t);
+                        .tickValues([1, 2.5, 4, 5.5, 7, 8.5, 10, 11.5, 13, 14.5])
+                        .tickFormat(t => t.toFixed(1));
 
         svgElement.append("g")
                     .attr("id", "legend")
                     .attr("transform", `translate(0, ${height - 40})`)
                     .call(xAxis)
                     .selectAll("rect")
-                    .data([2.5, 4, 5.5, 7, 8.5, 10, 11.5, 13])
+                    .data([1, 2.5, 4, 5.5, 7, 8.5, 10, 11.5, 13])
                     .enter()
                     .append("rect")
                     .attr("x", (d, i) => xScale(d))
                     .attr("y", -40)
-                    .attr("width", 35.5)
+                    .attr("width", 44)
                     .attr("height", 40)
                     .attr("stroke", "black")
-                    .attr("fill", (d, i) => COLORS[i]);
+                    .attr("fill", (d, i) => COLORS[i]["color"]);
     }
 
 }
--- js/main.js
+++ js/main.js
@@ -7,20 +7,18 @@ const showGraph = (dataset, baseTemperature) => {
     const PADDING = 150;
     let xScale = functions.createYearsScale(dataset, WIDTH, PADDING);
     let yScale = functions.createMonthsScale(HEIGHT, PADDING);
-    // Use dataset to build heatMap
-    // add legend color
-    functions.addTemperatureReference(svg, HEIGHT, PADDING);
-
     functions.addYearsAxis(svg, "years", xScale, (HEIGHT - PADDING));
     functions.addMonthsAxis(svg, "months", yScale, PADDING);
+    let graphData = functions.getGraphDataset(dataset, baseTemperature);
+    console.log(dataset);
+    console.log(graphData);
+    functions.buildHeatMap(svg, xScale, yScale, graphData);
+    functions.addTemperatureReference(svg, HEIGHT, PADDING);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
     const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
     fetch(url)
     .then(response => response.json())
-    .then(dataset => {
-        console.log(dataset);
-        showGraph(dataset.monthlyVariance, dataset.baseTemperature)
-    });
+    .then(dataset => showGraph(dataset.monthlyVariance, dataset.baseTemperature));
 });
